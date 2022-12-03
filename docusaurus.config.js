4e47b9bb2c0b17cb8c3e8de649e0ad82b90089da
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-12-03 15:03:40 +0000

    Add logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   // favicon: 'img/favicon.ico',
-  // staticDirectories: ['design-system/static'],
+  staticDirectories: ['static'],
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
