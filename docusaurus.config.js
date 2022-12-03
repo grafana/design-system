07db6732e58a36bf033825facef188d85774b82b
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-12-03 18:26:13 +0000

    Disable blog and tidy up files tree

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,12 +41,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false, // disabled for now but maybe we can have a design system blog later :) 
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
