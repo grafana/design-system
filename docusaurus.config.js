2d405da47063b247e3bc925a1e41d310ef619a33
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-12-05 17:43:34 +0000

    Update docs base path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
       ({
         docs: {
           path: 'docs',
-          routeBasePath: 'docs',
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
