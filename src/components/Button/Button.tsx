60a19f7095da7204698a0ead1dc4fc911e37c4e5
Joao Silva <joao.silva@grafana.com>
2023-08-10 16:13:50 +0100

    Token tweak

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,10 +5,10 @@ export const Button = ({ children }) => {
   return (
     <button
       style={{
-        backgroundColor: tokens.color.blue[400],
+        backgroundColor: tokens.button.primary.fill,
         border: 'none',
-        padding: `${tokens.spacing.x0_5} ${tokens.spacing.x1}`,
-        borderRadius: tokens.border.borderRadius.default,
+        padding: `${tokens.spacing.x1} ${tokens.spacing.x2}`,
+        borderRadius: tokens.button.primary.borderRadius,
         ...tokens.font.body,
         cursor: 'pointer',
       }}
