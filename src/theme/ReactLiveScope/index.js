5245265c9b3d4014335795239d36ca8a1fa4caef
joshhunt <josh@trtr.co>
2023-09-21 17:21:33 +0100

    comment

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -17,10 +17,12 @@ function Stack({ direction, gap, children }) {
   );
 }
 
+// Components must be added to this object in order for them to be "in scope" inside the live code previews
 const ReactLiveScope = {
   React,
   Badge,
   Stack,
   ...React,
 };
+
 export default ReactLiveScope;
