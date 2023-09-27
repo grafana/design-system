16790b4ecd654683e976da15867f597cf3751533
eledobleefe <laura.fernandez@grafana.com>
2023-09-27 12:02:25 +0200

    Add roadmap with achieved tasks marked

--- src/css/getStyles.ts
+++ src/css/getStyles.ts
@@ -325,6 +325,9 @@ export const getStyles = (theme: GrafanaTheme2) => {
       .two-column-div p:first-of-type {
         margin-right: 0;
       }
+    },
+    li.task-done {
+      text-decoration: line-through;
     }
   `;
 };
