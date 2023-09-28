ba5b691afd90713404e222e6e38a9d96cf8b2839
eledobleefe <laura.fernandez@grafana.com>
2023-09-28 12:32:13 +0200

    Update the url to match Overview page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
       navbar: {
         logo: {
           alt: 'Design System logo',
-          href: 'about',
+          href: 'About/overview',
           src: '/img/logo_with_text.png',
         },
         items: [
