d0b9a6098ac745deaef3728198bafc9af6eacc97
Clarity-89 <homes89@ukr.net>
2023-10-17 13:40:15 +0300

    Remove propFragment

--- src/components/ComponentEmbed.tsx
+++ src/components/ComponentEmbed.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export const ComponentEmbed = ({ path, propFragment, height = '300px' }) => {
+interface ComponentEmbedProps {
+  path: string;
+  height?: string;
+}
+export const ComponentEmbed = ({ path, height = '300px' }: ComponentEmbedProps) => {
   let iframeSrc = `https://developers.grafana.com/ui/canary/index.html?path=${path}&full=1&shortcuts=false&singleStory=true`;
-  if (propFragment) {
-    iframeSrc += `#${propFragment}`;
-  }
 
   return <iframe src={iframeSrc} width="100%" height={height} />;
 };
