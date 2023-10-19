0e66292e51885c18811a35b9278dbf2ec4a9d402
Joao Silva <100691367+JoaoSilvaGrafana@users.noreply.github.com>
2023-10-19 15:57:35 +0100

    Update docusaurus.config.dev.js
    Co-authored-by: Timur Olzhabayev <timur.olzhabayev@grafana.com>

--- docusaurus.config.dev.js
+++ docusaurus.config.dev.js
@@ -3,7 +3,7 @@ const baseConfig = require('./docusaurus.config.base');
 // /** @type {import('@docusaurus/types').Config} */
 const config = {
   ...baseConfig,
-  url: 'https://grafana-dev.com',
+  url: 'https://grafana-dev.com/',
   baseUrl: 'developers/saga/',
 };
 
