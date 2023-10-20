5ee30b123b19327c50b33800a193ae3ef58b6144
Joao Silva <joao.silva@grafana.com>
2023-10-20 16:56:22 +0100

    Saga: Add cookie consent banner

--- docusaurus.config.dev.js
+++ docusaurus.config.dev.js
@@ -5,6 +5,16 @@ const config = {
   ...baseConfig,
   url: 'https://grafana-dev.com/',
   baseUrl: 'developers/saga/',
+
+  customFields: {
+    rudderStackTracking: {
+      url: 'https://rs.grafana-dev.com',
+      writeKey: '1w02fcWseyqcwsJA9CSKRkfEOfU',
+      configUrl: 'https://rsc.grafana.com',
+      sdkUrl: 'https://rsdk.grafana.com',
+    },
+    canSpamUrl: 'https://grafana-dev.com/canspam',
+  },
 };
 
 module.exports = config;
