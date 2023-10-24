3302174303ffb532656f1406119eb2097a8dc38a
eledobleefe <laura.fernandez@grafana.com>
2023-10-24 15:43:13 +0200

    Add icon according to status

--- src/theme/StatusTable/index.tsx
+++ src/theme/StatusTable/index.tsx
@@ -1,4 +1,4 @@
-import { IconName, InteractiveTable } from '@grafana/ui';
+import { CellProps, Icon, IconName, InteractiveTable } from '@grafana/ui';
 import React, { useMemo } from 'react';
 
 interface StatusTableProps {
@@ -18,79 +18,116 @@ interface Status {
   className: string;
 }
 
-// const STATUSES: Status[] = [
-//   {
-//     id: 'done',
-//     status: 'Done',
-//     icon: 'check-circle',
-//     className: 'done',
-//   },
-//   {
-//     id: 'planned',
-//     status: 'Planned',
-//     icon: 'calendar-alt',
-//     className: 'planned',
-//   },
-//   {
-//     id: 'in-progress',
-//     status: 'In Progress',
-//     icon: 'sync',
-//     className: 'in-progress',
-//   },
-//   {
-//     id: 'to-do',
-//     status: 'To Do',
-//     icon: 'list-ul',
-//     className: 'to-do',
-//   },
-//   {
-//     id: 'not-doing',
-//     status: 'Not Doing',
-//     icon: 'times',
-//     className: 'not-doing',
-//   },
-//   {
-//     id: 'deprecated',
-//     status: 'Deprecated',
-//     icon: 'minus-circle',
-//     className: 'deprecated',
-//   },
-//   {
-//     id: 'n-a',
-//     status: 'N/A',
-//     icon: 'question-circle',
-//     className: 'n-a',
-//   },
-// ];
+const STATUSES: Status[] = [
+  {
+    id: 'done',
+    status: 'Done',
+    icon: 'check-circle',
+    className: 'done',
+  },
+  {
+    id: 'planned',
+    status: 'Planned',
+    icon: 'calendar-alt',
+    className: 'planned',
+  },
+  {
+    id: 'in-progress',
+    status: 'In Progress',
+    icon: 'sync',
+    className: 'in-progress',
+  },
+  {
+    id: 'to-do',
+    status: 'To Do',
+    icon: 'list-ul',
+    className: 'to-do',
+  },
+  {
+    id: 'not-doing',
+    status: 'Not Doing',
+    icon: 'times',
+    className: 'not-doing',
+  },
+  {
+    id: 'deprecated',
+    status: 'Deprecated',
+    icon: 'minus-circle',
+    className: 'deprecated',
+  },
+  {
+    id: 'n-a',
+    status: 'N/A',
+    icon: 'question-circle',
+    className: 'n-a',
+  },
+];
+
+const columns = [
+  {
+    id: 'componentName',
+    title: 'Component',
+    header: 'Component name',
+  },
+  {
+    id: 'generalStatus',
+    title: 'General',
+    header: 'General status',
+    cell: ({ cell: { value } }: CellProps<Status>) => {
+      const { icon, status, className } = STATUSES.find((status) => status.id === value) || STATUSES[STATUSES.length - 1];
+      return (
+        <span className={className}>
+          <Icon name={icon}/>
+          {status}
+        </span>
+      );
+    },
+  },
+  {
+    id: 'figmaStatus',
+    title: 'Figma',
+    header: 'Figma status',
+    cell: ({ cell: { value } }: CellProps<Status>) => {
+      const { icon, status, className } = STATUSES.find((status) => status.id === value) || STATUSES[STATUSES.length - 1];
+      return (
+        <span className={className}>
+          <Icon name={icon}/>
+          {status}
+        </span>
+      );
+    },
+  },
+  {
+    id: 'storybookStatus',
+    title: 'Storybook',
+    header: 'Storybook status',
+    cell: ({ cell: { value } }: CellProps<Status>) => {
+      const { icon, status, className } = STATUSES.find((status) => status.id === value) || STATUSES[STATUSES.length - 1];
+      return (
+        <span className={className}>
+          <Icon name={icon}/>
+          {status}
+        </span>
+      );
+    },
+  },
+  {
+    id: 'sagaStatus',
+    title: 'Saga',
+    header: 'Saga status',
+    cell: ({ cell: { value } }: CellProps<Status>) => {
+      const { icon, status, className } = STATUSES.find((status) => status.id === value) || STATUSES[STATUSES.length - 1];
+      return (
+        <span className={className}>
+          <Icon name={icon}/>
+          {status}
+        </span>
+      );
+    },
+  },
+];
 
 export const StatusTable = ({ componentsData }: StatusTableProps) => {
-  const columns = [
-    {
-      id: 'componentName',
-      title: 'Component',
-      header: 'Component name',
-    },
-    {
-      id: 'generalStatus',
-      title: 'General',
-      header: 'General status',
-    },
-    {
-      id: 'figmaStatus',
-      title: 'Figma',
-      header: 'Figma status',
-    },
-    {
-      id: 'storybookStatus',
-      title: 'Storybook',
-      header: 'Storybook status',
-    },
-    {
-      id: 'sagaStatus',
-      title: 'Saga',
-      header: 'Saga status',
-    },
-  ];
   const rowsData = useMemo(() => componentsData, [componentsData]);
   return <InteractiveTable getRowId={(row: StatusChildren) => row.componentName} columns={columns} data={rowsData} />;
 };
