8a4ff186de5bd06c3de966ba4727804b767ccdad
Clarity-89 <homes89@ukr.net>
2023-11-07 13:45:53 +0200

    Re-add deleted component

---
+++ src/components/ComponentEmbed.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ComponentEmbedProps {
+  path: string;
+  height?: string;
+}
+export const ComponentEmbed = ({ path, height = '300px' }: ComponentEmbedProps) => {
+  let iframeSrc = `https://developers.grafana.com/ui/canary/index.html?path=${path}&full=1&shortcuts=false&singleStory=true`;
+
+  return <iframe src={iframeSrc} width="100%" height={height} />;
+};
