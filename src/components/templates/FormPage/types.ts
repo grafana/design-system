16f8324cea6cc64325d44a337532d93bcdf1fc3c
Clarity-89 <homes89@ukr.net>
2023-11-23 16:32:53 +0200

    Fix types

--- src/components/templates/FormPage/types.ts
+++ src/components/templates/FormPage/types.ts
@@ -1,10 +1,12 @@
+import { SelectableValue } from '@grafana/data';
+
 export type Data = {
   text: string;
   textarea: string;
   number: number;
   checkbox: boolean;
   radio: string;
-  select: string;
+  select: SelectableValue<string>;
   switch: boolean;
   slider: number;
 };
