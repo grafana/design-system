58e51a123c5d97121519fb17da293d5df3573f6b
Joao Silva <joao.silva@grafana.com>
2024-03-21 11:53:26 +0000

    Move trailing slash config to base

--- docusaurus.config.base.js
+++ docusaurus.config.base.js
@@ -13,6 +13,7 @@ const baseConfig = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/logo.png',
   staticDirectories: ['static'],
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
